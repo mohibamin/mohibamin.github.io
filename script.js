8dfb99a116c782d321e8b1bdb96db2383b6d288e
Mohib Amin <129216337+mohibamin@users.noreply.github.com>
2026-02-06 22:34:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -186,6 +186,11 @@ document.querySelectorAll(".gallery").forEach(gallery => {
   updateGallery();
 });
 
+const mobileToggle = document.getElementById("mobileToggle");
+
+mobileToggle.addEventListener("click", () => {
+  document.body.classList.toggle("mobile-nav-open");
+});
 
 
 
